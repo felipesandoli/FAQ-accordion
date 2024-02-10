e4779df5bfde6a5023645a08377f727462af08b9
felipesandoli <cfelipesandoli@gmail.com>
2024-02-10 17:41:37 +0000

    Add other asnwer when a new faq is selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const faqItems = document.getElementsByClassName("faq-item")
+const faqItems = document.getElementsByClassName("faq-item");
 
 for (var i = 0; i < faqItems.length; i++) {
     faqItems[i].addEventListener('click', toggle);
@@ -7,12 +7,21 @@ for (var i = 0; i < faqItems.length; i++) {
 function toggle() {
     let faq = this.children
     if (faq[1].classList.contains("d-none")) {
+        let faqItems = document.getElementsByClassName("faq-item");
+        for (var i = 0; i < faqItems.length; i++) {
+            let question = faqItems[i].children[0];
+            let answer = faqItems[i].children[1];
+            if(!answer.classList.contains("d-none")) {
+                answer.classList.add("d-none");
+                let toggleSign = question.children[1];
+                toggleSign.src = "assets/images/icon-plus.svg";
+            }
+        }
         faq[1].classList.remove("d-none");
         let toggleSign = faq[0].children[1];
         toggleSign.src = "assets/images/icon-minus.svg";
     }
     else {
-        console.log(faq[1].classList)
         faq[1].classList.add("d-none");
         let toggleSign = faq[0].children[1];
         toggleSign.src = "assets/images/icon-plus.svg";
